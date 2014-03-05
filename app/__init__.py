0be26f14548d2a023b98a4deaa91e0f13b91dce6
Nikita Pekin <nikitapekin@gmail.com>
2014-03-05 00:04:45 -0500

    Changes

--- app/__init__.py
+++ app/__init__.py
@@ -7,7 +7,7 @@ from flask import Flask
 app = Flask(__name__)
 
 # Load the configuration.
-app.config.from_object('config.DevelopmentConfig')
+app.config.from_object('config.ProductionConfig')
 
 # Register blueprints.
 app.register_blueprint(views.mod)
