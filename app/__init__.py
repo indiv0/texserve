9c02c8b09ffdf517d13a0bd6ceff1ab73a449590
Nikita Pekin <nikitapekin@gmail.com>
2014-03-07 02:23:45 -0500

    Change the way notes.json is handled

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,8 @@ from app import views
 
 from flask import Flask
 
+import json
+
 # Initialize the app.
 app = Flask(__name__)
 
@@ -14,3 +16,13 @@ app.register_blueprint(views.mod)
 
 # Create a representation of the S3 bucket.
 bucket = s3.Bucket(config.BUCKET_NAME, config.AWS_ACCESS_KEY_ID, config.AWS_SECRET_ACCESS_KEY)
+
+# Load JSON stored note information.
+print('Loading JSON data on notes from file.')
+note_data = json.load(open('notes.json'))
+# Retrieve 'notes.json' from the bucket, and write it if it doesn't exist.
+try:
+    note_data = bucket.getFile('notes.json')
+except:
+    print('Uploading updated JSON data on notes to S3 bucket.')
+    bucket.uploadFile('notes.json')
